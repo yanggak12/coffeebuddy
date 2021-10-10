20364c6746fb3bf7b98733d1b05be58366c2a91a
양윤상 <yanggak12@naver.com>
2021-10-10 15:40:02 +0900

    feat : nav router

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <nav id="nav-router">
+        <div id="nav-router-links">
+          <router-link to="/" tag="div" id="logo"><img src="../public/logo.png"></router-link>
+          <router-link to="/coffee">Coffee</router-link>
+          <router-link to="/beans">Beans</router-link>
+          <router-link to="/buddy">Buddy</router-link>
+          <router-link to="/community">Community</router-link>
+        </div>
+      </nav>
+      <nav id="nav-sign">
+        <div id="nav-router-links">
+        <router-link to="/signin" tag="button" id="sign-in">Sign In</router-link>
+        <router-link to="/signup" tag="button" id="sign-up">Sign Up</router-link>
+        </div>
+      </nav>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+ 
+}
+</script>
+
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html{
+  font-size: 14px;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+body{
+  background: #f0efe1;
+  overflow-x: hidden;
+  letter-spacing: -0.05em;
+}
+a {
+  color:#996600;
+  text-decoration: none;
+}
+/* nav */
+.nav {
+  background: #fffeef;
+  display: flex;
+}
+#nav-router {
+  width: 100%;
+  height: 5rem;
+  padding: 0 1rem;
+  font-size: 0.8rem;
+}
+#nav-router-links{
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+img {
+  width: 3rem;
+  height: 3rem;
+}
+#nav-sign {
+  display: flex;
+  width: 40%;
+  justify-content: flex-end;
+}
+#nav-sign button{
+  width: 3rem;
+  height: 2rem;
+  border-radius: 0.8em;
+  cursor: pointer;
+  font-size: 0.7rem;
+  margin-right: 0.5rem;
+}
+#sign-in {
+  background: #fff;
+  border: #996600 1px solid;
+  color:#996600;
+}
+#sign-up {
+  background: #996600;
+  border: none;
+  color:#fff;
+}
+@media (min-width: 1024px) {
+  html{
+    font-size: 20px;
+  }
+  img {
+    width: 5rem;
+    height: 5rem;
+  }
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Coffee from "../views/Coffee.vue";
+import Beans from "../views/Beans.vue";
+import Buddy from "../views/Buddy.vue";
+import Commnunity from "../views/Community.vue";
+import SignIn from "../views/SignIn.vue";
+import SignUp from "../views/SignUp.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +17,34 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/coffee",
+    name: "Coffee",
+    component: Coffee,
+  },
+  {
+    path: "/beans",
+    name: "Beans",
+    component: Beans,
+  },
+  {
+    path: "/buddy",
+    name: "Buddy",
+    component: Buddy,
+  },
+  {
+    path: "/community",
+    name: "Commnunity",
+    component: Commnunity,
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
   },
 ];
 
